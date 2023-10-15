57f9b92f6553edb1c52d54708833e51faf24a8a0
Vitalii Natalevych <vnatalevic@gmail.com>
2023-10-16 00:48:38 +0200

    1.1

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
 
+template <class  T>
 
 class Node{
 private:
     int id;
 public:
 
-    Node * next;
-    Node * prev;
-    int data;
+    Node<T> * next;
+    Node<T> * prev;
+    T data;
 
-    Node(int value) {
+    Node<T>(T value) {
         id  = 0;
         next = nullptr;
         prev = nullptr;
@@ -40,74 +43,53 @@ public:
 
 };
 
-void swapPointers(Node * first, Node * second){
-    auto pSwap = *first;
-    *first = *second;
-    *second = pSwap;
-}
 
-bool compare(Node *first, Node *second){
-    return first->data - second->data < 0;
+template <class  T>
+
+int compare(T first, T second){
+    return first - second < 0 ? -1 :first - second == 0? 0 : 1;
 }
 
 
-class linkedList{
-
+template <class  T>
+class linkedList {
 private:
     int size;
-    Node *head;
-    Node *tail;
+    Node<T> *head;
+    Node<T> *tail;
     int idQty;
 
 
 public:
-//    void setValue(T value){
-//        this->value = value;
-//    }
-//    T getValue(){
-//        return this->value;
-//    }
-//    void setPrev( linkedList<T>  * list){
-//        this->prev = list;
-//    }
-//    linkedList<T> * getPrev(){
-//        return this->prev;
-//    }
-//    void setNext(linkedList<T> *list){
-//        this->next = list;
-//    }
-//    linkedList<T> * getNext(){
-//        return this->next;
-//    }
-//
-////    void setIndex(){
-////        this->index = 0;
-////
-////    }
 
 
-    linkedList() {
+    linkedList<T>() {
         idQty = 1;
         size = 0;
         head = nullptr;
         tail = nullptr;
     }
 
-    Node * getHead(){
-    return this->head;
-    }
-    int getSize(){
-    return this->size;
+    ~linkedList<T>() {
+        this->clearList();
     }
 
-    void add_begin(int value){
+    Node<T> *getHead() {
+        return this->head;
+    }
 
-        Node * newNode = new Node(value);
+    int getSize() {
+        return this->size;
+    }
+
+    void add_begin(T value) {
+
+        auto *newNode = new Node<T>(value);
         newNode->next = this->head;
 
-        if(this->size == 0){
+        if (this->size == 0) {
             this->tail = newNode;
-        }else{
+        } else {
             this->head->prev = newNode;
         }
         this->head = newNode;
@@ -118,14 +100,15 @@ public:
 
 
     }
-    void push_tail(int value){
-        Node * newNode = new Node(value);
+
+    void push_tail(T value) {
+        auto *newNode = new Node<T>(value);
         newNode->prev = this->tail;
 
 
-        if(size < 1){
+        if (size < 1) {
             this->head = newNode;
-        }else{
+        } else {
             this->tail->next = newNode;
         }
 
@@ -136,62 +119,61 @@ public:
         this->size++;
     }
 
-    Node * searchNode(int value){
-        auto * tmp = this->getHead();
-        while(tmp != nullptr){
-           if(tmp->data == value){
-               return tmp;
-           }
+    Node<T> *searchNode(T value) {
+        auto *tmp = this->getHead();
+        while (tmp != nullptr) {
+            if (tmp->data == value) {
+                return tmp;
+            }
             tmp = tmp->next;
         }
         return nullptr;
     }
 
-    void deleteLast(){
-    this->tail = this->tail->prev;
-    delete this->tail->next;
-    this->tail->next = nullptr;
-    this->size--;
-
-    }
-
-    void deleteFirst(){
-    this->head = this->head->next;
-    delete this->head->prev;
-    this->head->prev = nullptr;
-    this->size--;
-    }
-
-    void deleteNodeValue(int value){
-    if(searchNode(value)){
-        searchNode(value)->next->prev = searchNode(value)->prev;
-        searchNode(value)->prev->next = searchNode(value)->next;
+    void deleteLast() {
+        this->tail = this->tail->prev;
+        delete this->tail->next;
+        this->tail->next = nullptr;
         this->size--;
-        cout<<"Success"<<endl;
-    }
-    else{
-        cout<< "Failed: invalid value"<<endl;
-    }
 
     }
-    void deleteNodeIndex(int index){
-        if(this->index(index)){
+
+    void deleteFirst() {
+        this->head = this->head->next;
+        delete this->head->prev;
+        this->head->prev = nullptr;
+        this->size--;
+    }
+
+    void deleteNodeValue(T value) {
+        if (searchNode(value)) {
+            searchNode(value)->next->prev = searchNode(value)->prev;
+            searchNode(value)->prev->next = searchNode(value)->next;
+            this->size--;
+            cout << "Success" << endl;
+        } else {
+            cout << "Failed: invalid value" << endl;
+        }
+
+    }
+
+    void deleteNodeIndex(int index) {
+        if (this->index(index)) {
             this->index(index)->next->prev = this->index(index)->prev;
             this->index(index)->prev->next = this->index(index)->next;
             this->size--;
-            cout<<"Success"<<endl;
-        }
-        else{
-            cout<< "Failed: invalid value"<<endl;
-        }
+            cout << "Success" << endl;
 
+        } else {
+            cout << "Failed: invalid value" << endl;
+        }
     }
 
 
-    Node * index(int i){
-        auto * tmp = this->getHead();
-        for(int j = 0; j<this->size; j++){
-            if(j == i){
+    Node<T> *index(int i) {
+        auto *tmp = this->getHead();
+        for (int j = 0; j < this->size; j++) {
+            if (j == i) {
                 return tmp;
             }
             tmp = tmp->next;
@@ -200,101 +182,118 @@ public:
         return nullptr;
     }
 
-    void newValue(int index, int value){
-        if(this->index(index)){
+    void newValue(int index, T value) {
+        if (this->index(index)) {
             this->index(index)->data = value;
-            cout<<"Success"<<endl;
-        }else{
-            cout<<"Failed: invalid index"<<endl;
+            cout << "Success" << endl;
+        } else {
+            cout << "Failed: invalid index" << endl;
         }
     }
-    void clearList(){
-        while(size != 0){
-            if(this->size == 1){
+
+    void clearList() {
+        while (size != 0) {
+            if (this->size == 1) {
                 delete this->head;
                 this->head = nullptr;
                 this->tail = nullptr;
                 this->size = 0;
-            }else{
+            } else {
                 this->deleteFirst();
             }
-         }
-    }
-
-
-
-    void sort(){
-        bool change;
-        for(int i = 0; i<size-1;i++){
-            change = false;
-            for(int j = 0; j<size-i-1; j++){
-                if(compare(this->index(j),this->index(j+1))){
-                   // swap(j, j+1);
-                    change = true;
-                }
-            }
-            if(change) return;
         }
     }
 
-    void printList(){
-        auto * tmp = this->getHead();
-        if(tmp == nullptr){
-            cout<<"Empty list"<<endl;
+
+    void printList() {
+        auto *tmp = this->getHead();
+        if (tmp == nullptr) {
+            cout << "Empty list" << endl;
             return;
         }
-        while(tmp != nullptr){
+        while (tmp != nullptr) {
             tmp->printNode();
             tmp = tmp->next;
         }
     }
 
+    void orderedInsert(T value) {
+
+        if(!size){
+            add_begin(value);
+            return;
+        }
+
+
+        Node<T> *tmp = this->head;
+
+
+
+        if (compare(this->head->data, value) >= 1) {
+            this->add_begin(value);
+            return;
+        }
+        auto *newNode = new Node(value);
+        while(true){
+        if (compare(tmp->data, value) >= 0) {
+            newNode->prev = tmp->prev;
+            auto tmp1 = tmp->prev;
+            newNode->next = tmp;
+            tmp->prev = newNode;
+            tmp1->next = newNode;
+            this->size++;
+            newNode->setId(this->idQty);
+            this->idQty++;
+            return;
+        }else{
+            if(tmp->next){
+                tmp = tmp->next;
+            }else{
+                this->push_tail(value);
+                return;
+            }
+
+        }}
+
+    };
 };
 
-
- int getRandom(){
-
+template <class T>
+ T getRandom(){
+    return rand() % 1000;
  }
 
 
 
 int main() {
-    auto * first = new linkedList();
-    first->add_begin(14);
-    first->add_begin(15);
-    first->add_begin(14);
-    first->add_begin(13);
-    first->add_begin(19);
-    first->push_tail(1);
-    first->push_tail(8);
-    first->push_tail(9);
-    first->deleteLast();
-    first->push_tail(20);
+    auto * first = new linkedList<int>();
+    int Qty = 1000;
+    //first->add_begin(getRandom<int>());
 
+    clock_t t1 = clock();
 
-
+    for(int i = 0; i<61; i++) {
+        first->orderedInsert(getRandom<int>());
+    }
     first->printList();
-    cout<< first->getHead()->data<<endl;
-    first->searchNode(13)->printNode();
-    cout<<first->getSize() << endl;
-    first->deleteNodeValue(14);
-    first->printList();
-    cout<<endl;
-    first->index(11)->printNode();
+        clock_t t2 = clock();
+    double duration = double(t2-t1)/CLOCKS_PER_SEC;
+    cout << duration;
+
+//    cout<< first->getHead()->data<<endl;
+//    first->searchNode(13)->printNode();
+//    cout<<first->getSize() << endl;
+//    first->deleteNodeValue(14);
+//    cout<<endl;
+//    first->index(11)->printNode();
     //first->clearList();
-
-    cout<<endl;
-    first->printList();
-
-    cout<<endl;
-    //first->swap(1,2);
-    //first->sort();
-    //first->printList();
-    cout<<endl;
-    auto  * p = new Node(7);
-    auto * q = new Node(9);
-    swapPointers(p,q);
-     p->printNode() ;
-     q->printNode();
+//
+//    cout<<endl;
+//
+//    cout<<endl;
+//    first->deleteNodeValue(145);
+//    first->orderedInsert(505);
+//
+//    first->printList();
     return 0;
 }
